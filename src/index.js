e2647a206c7a0254a0815d1d55b2d3a3f8d78572
Vinicius <viniciusmoura761@gmail.com>
2025-05-14 20:36:24 -0300

    feat: add new item function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,28 @@ import express, { json } from "express";
 import cors from "cors"
 
 const server = express();
+
 server.use(cors());
 server.use(json());
+
 server.listen(5000, () => {
   console.log('Servidor iniciado na porta 5000');
 });
+
+const lista = [];
+
+server.post('/items', (req, res) => {
+  const {name, quantity, type} = req.body;
+  if (!name || !quantity || !type)  return res.status(422).send('Preencha todos os campos! (name, quantity e type)');
+  lista.forEach(item => {
+    if (item.name === name) return res.status(409).send('Item já cadastrado');
+  })
+  const novoItem = {
+    id : lista.length + 1,
+    name,
+    quantity,
+    type
+  }
+  lista.push(novoItem);
+  res.status(201).send(novoItem);
+})
