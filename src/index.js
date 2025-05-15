cb4a0763f914d777110a09799e93f4c4fe95b227
Vinicius <viniciusmoura761@gmail.com>
2025-05-15 20:41:54 -0300

    feat: add semantic package for status code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import cors from "cors"
+import status from "http-status";
 
 const server = express();
 
@@ -14,9 +15,9 @@ const lista = [];
 
 server.post('/items', (req, res) => {
   const {name, quantity, type} = req.body;
-  if (!name || !quantity || !type)  return res.status(422).send('Preencha todos os campos! (name, quantity e type)');
+  if (!name || !quantity || !type)  return res.status(status.UNPROCESSABLE_ENTITY).send({ message: "Preencha todos os campos!" });
   const itemExistente = lista.find(item => item.name === name);
-  if (itemExistente) return res.status(409).send('Item já cadastrado');
+  if (itemExistente) return res.status(status.CONFLICT).send({ message: "Item já cadastrado" });
   const novoItem = {
     id : lista.length + 1,
     name,
@@ -24,22 +25,22 @@ server.post('/items', (req, res) => {
     type
   }
   lista.push(novoItem);
-  res.status(201).send(novoItem);
+  res.status(status.CREATED).send(novoItem);
 })
 
 server.get('/items', (req, res) => {
   const { type } = req.query
   if (type) {
     const filtrados = lista.filter(item => item.type === type);
-    res.send(filtrados);
+    return res.status(status.OK).send(filtrados);
   }
-  res.send(lista);
+  res.status(status.OK).send(lista);
 })
 
 server.get('/items/:id', (req, res) => {
   const { id } = req.params;
-  if (id <= 0) return res.status(400).send('Id inválido!');
+  if (id <= 0) return res.status(status.BAD_REQUEST).send({ message: 'Id inválido!' });
   const item = lista.find(item => item.id === Number(id));
-  if (item) return res.send(item);
-  else res.status(404).send('Id não encontrado!');
+  if (item) return res.status(status.OK).send(item);
+  else res.status(status.NOT_FOUND).send({ message: 'Id não encontrado!' });
 })
